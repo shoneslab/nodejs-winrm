ada09abe4ddc7ce12f2b0176459c6fabb9957829
Shone Jacob <shone.jacob@gmail.com>
2018-12-10 09:48:00 -0500

    Remove Path from parameters

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let winrm_execute_cmd = require('./src/execute-command.js');
 let winrm_receive_output = require('./src/receive-output.js');
 let winrm_delete_shell = require('./src/delete-shell.js');
 
-async function shell(_command, _host, _port, _path, _username, _password) {
+async function shell(_command, _host, _port, _username, _password) {
     var auth = 'Basic ' + Buffer.from(_username + ':' + _password, 'utf8').toString('base64');
     var params = {
         host: _host,
@@ -19,8 +19,11 @@ async function shell(_command, _host, _port, _path, _username, _password) {
 
     params['commandId'] = commandId;
     var output = await winrm_receive_output.doReceiveOutput(params);
+    console.log(output)
 
     var cleanup = await winrm_delete_shell.doDeleteShell(params);
+
+    return output
 }
 
 module.exports = {
